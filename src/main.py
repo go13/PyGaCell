e465a5ab01db43c74403b7d0518cc9f195bb2695
dmytro <dima.blinkov@gmail.com>
2016-10-30 14:49:20 +0000

    improvement

--- src/main.py
+++ src/main.py
@@ -7,27 +7,28 @@ print("PyGACell")
 
 
 def fn(cell, experiment_number):
-    x = (random.random() - 0.5) * 4
+    x = (random.random() - 0.5) * 10
     cell.set_inputs([x, x])
 
     res = cell.calc()[0]
 
-    y = - x * x + 4
+    y = -x * x - 4
 
-    rating = exp(-(res - y) * (res - y))
+    rating = exp(-(res - y) * (res - y) / 2)
 
     #if experiment_number == 1:
     #    print "x = ", x, " y = ", y, " res = ", res, " rating = ", rating
 
     return rating
 
-ga = GA(Params(2, 1, fn, 20, 4, 1, 100))
+ga = GA(Params(2, 1, fn, 50, 20, 1, 10, 10, 100))
 
 for i in range(1000):
     print "step - ", i
     ga.calc()
 
-    print ga.population[0].rating
-
+    cell = ga.population[0]
+    print "rating = ", cell.pure_rating, " hub number = ", cell.get_hub_number()
+    print cell
     ga.grow()
     print
